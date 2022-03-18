fc8da8177ae1cea910f6546106ce9b3872dcd719
Kyle Kotowick <kotowick@invictonlabs.com>
2022-03-18 16:57:34 -0400

    test

--- terraform/lambda-shell.tf
+++ terraform/lambda-shell.tf
@@ -17,7 +17,7 @@ module "lambda_shell_layer_permission_granter" {
     aws = aws.ca_central_1
   }
   lambda_memory_size = 128
-  lambda_timeout     = 30
+  lambda_timeouts    = 30
   lambda_role_policies_json = [
     data.aws_iam_policy_document.layer_permission_granter.json
   ]
