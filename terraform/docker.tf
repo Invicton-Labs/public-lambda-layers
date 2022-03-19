534bdfbc90342e4eee3715b75805f7097fc8a887
Kyle Kotowick <kotowick@invictonlabs.com>
2022-03-19 17:01:33 -0400

    Update docker.tf

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -123,9 +123,8 @@ module "create_dockerfiles" {
 
 // Build the Docker containers that we will pull the layer files from
 module "docker_build" {
-  //source   = "Invicton-Labs/shell-resource/external"
-  //version  = "~>0.3.1"
-  source = "../../../terraform-modules-public/terraform-external-shell-resource"
+  source   = "Invicton-Labs/shell-resource/external"
+  version  = "~>0.3.2"
   for_each = local.docker_images
   depends_on = [
     module.create_builder,
